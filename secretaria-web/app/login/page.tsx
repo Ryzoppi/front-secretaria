a96675d9f16c394a89f2d222f0d387b7afc6346a
AllanShima <allanshinhama@gmail.com>
2026-09-04 20:38:08 -0300

    add: links para redirecionamento cliend-side

--- secretaria-web/app/login/page.tsx
+++ secretaria-web/app/login/page.tsx
@@ -1,9 +1,10 @@
+import Link from 'next/link'
 import React from 'react'
 
 const UserLogin = () => {
   return (
-    <div>
-      
+    <div className='p-4'>
+      <Link href={"/register"}>Não possui uma conta? Cadastre-se</Link>
     </div>
   )
 }
--- secretaria-web/app/register/page.tsx
+++ secretaria-web/app/register/page.tsx
@@ -1,9 +1,10 @@
+import Link from 'next/link'
 import React from 'react'
 
 const UserRegister = () => {
   return (
-    <div>
-      
+    <div className='p-4'>
+      <Link href={"/login"}>Já possui uma conta? Faça o Login</Link>
     </div>
   )
 }
