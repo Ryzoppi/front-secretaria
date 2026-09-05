847e92dc062fa083f8b1eb9a3e42e94887b7dd8b
AllanShima <allanshinhama@gmail.com>
2026-09-04 21:22:39 -0300

    add: fetch e caching

--- secretaria-web/app/login/page.tsx
+++ secretaria-web/app/login/page.tsx
@@ -1,11 +1,30 @@
 import Link from 'next/link'
 import React from 'react'
 
-const UserLogin = () => {
+interface User {
+  id: string;
+  name: string;
+
+}
+
+const UserLogin = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", { next: {revalidate: 10}}); // a cada 10s, re-fetch
+  const users: User[] = await res.json();
+
   return (
+    
     <div className='p-4'>
-      <Link href={"/register"}>Não possui uma conta? Cadastre-se</Link>
+      <h1 className='text-blue-500'>Usuários logados (teste de request)</h1>
+      <ul>
+        {users.map(user => 
+        <li key={user.id}>
+          {user.name}
+        </li>
+      )}
+      </ul>
+      <Link href={"/register"} className='text-red-500'>Não possui uma conta? Cadastre-se</Link>
     </div>
+
   )
 }
 
