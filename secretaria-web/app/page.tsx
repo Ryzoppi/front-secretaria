38f1825ffa0ad6eb53dbe5ec227c7e1a68f0b5cf
AllanShima <allanshinhama@gmail.com>
2026-09-16 20:11:24 -0300

    fix: conflito

--- secretaria-web/app/page.tsx
+++ secretaria-web/app/page.tsx
@@ -3,22 +3,18 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
     <main className="p-4 bg-amber-400">
-      <h1>Hello world!</h1>
+      <div className="p-4">
+        <main className="w-full flex justify-between">
+          <h1>
+            Landing Page
+          </h1>
+          <span className="flex gap-4">
+            <Link href={"/login"}>Login</Link>
+            <Link href={"/register"}>Cadastro</Link>          
+          </span>
+        </main>
+      </div>
     </main>
-=======
-    <div className="p-4">
-      <main className="w-full flex justify-between">
-        <h1>
-          Landing Page
-        </h1>
-        <span className="flex gap-4">
-          <Link href={"/login"}>Login</Link>
-          <Link href={"/register"}>Cadastro</Link>          
-        </span>
-      </main>
-    </div>
->>>>>>> estudando-nextjs
   );
 }
